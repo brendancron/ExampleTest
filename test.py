d18118e1d4d938cd63df3bf920479a01654a3814
Brendan Cron <brendancollege85@gmail.com>
2020-08-02 14:56:15 -0400

    added unit tests

---
+++ test.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.insert(1, '../code')
+import code as calc
+import unittest
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        result = calc.add(3,4)
+        self.assertEqual(result, 7)
+
+    def test_subtract(self):
+        result = calc.subtract(9,7)
+        self.assertEqual(result, 2)
+
+    def test_multiply(self):
+        result = calc.multiply(4,5)
+        self.assertEqual(result, 20)
+
+    def test_div(self):
+        result = calc.divide(30,5)
+        self.assertEqual(result, 6)
+
+if (__name__ == "__main__"):
+    unittest.main()
